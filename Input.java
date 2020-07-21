34528b930759ee79e40efbcde942a4c2c6df4406
Aarif Ali Saifi <67059471+MrAarifAliSaifi@users.noreply.github.com>
2020-07-21 13:25:19 +0530

    prgram to take the integer value from the user

---
+++ Input.java
@@ -0,0 +1,14 @@
+package Start;
+
+import java.util.Scanner;
+
+public class Input {
+
+	public static void main(String[] args) {
+	Scanner sc=new Scanner(System.in);
+	int num;
+	System.out.println("Enter the value");
+	num=sc.nextInt();
+	System.out.println("The value which user enterdis:"+num);
+	}
+}
