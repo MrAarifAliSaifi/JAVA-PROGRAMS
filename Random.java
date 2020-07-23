2952a50328f3454762f066a2bf12987e95a87a58
Aarif Ali Saifi <67059471+MrAarifAliSaifi@users.noreply.github.com>
2020-07-23 15:57:01 +0530

    TO GENERATE THE RANDOM NO IN JAVA

---
+++ Random.java
@@ -0,0 +1,10 @@
+package Start;
+import java.lang.Math;
+public class GeneratingRandomNumber {
+
+	public static void main(String[] args) {
+		System.out.println("The first gerationg nois:"+Math.random());
+	
+	}
+
+}
