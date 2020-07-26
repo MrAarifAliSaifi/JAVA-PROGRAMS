39c5e539bd8f2901439f036262b65b825dc2d33c
Aarif Ali Saifi <67059471+MrAarifAliSaifi@users.noreply.github.com>
2020-07-26 14:50:48 +0530

    SHORTING IN JAVA

---
+++ shorting.java
@@ -0,0 +1,29 @@
+package arrayBasic;
+
+public class Shorting {
+
+	public static void main(String[] args) {
+		int[] arr= {5,6,3,2,6,25,6,8,};
+		int temp=0;
+		for(int i=0;i<arr.length;i++)
+		{
+			for(int j=i+1;j<arr.length;j++)
+			{
+				if(arr[i]>arr[j])
+				{
+					 temp=arr[i];
+					arr[i]=arr[j];
+					arr[j]=temp;
+				}
+				
+			}
+			
+		}
+		for(int i:arr)
+		{
+			System.out.println(i);
+		}
+			
+
+}
+}
